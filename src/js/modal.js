686b2d13ad0f00683781758e0813a46a9476dff5
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2022-12-23 12:04:44 +0200

    fixing data in products

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-products-open]'),
+    closeModalBtn: document.querySelector('[data-modal-products-close]'),
+    modal: document.querySelector('[data-modal-products]'),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
